ecd2d6e39a4a9729156ad4eb6b7fbde91ecfcb17
Chik3r <31808958+Chik3r@users.noreply.github.com>
2024-10-08 16:01:46 +0200

    frame

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        JFrame frame = new JFrame();
+
+        frame.setSize(1024, 768);
+        frame.setResizable(false);
+        frame.setLayout(null);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 }
